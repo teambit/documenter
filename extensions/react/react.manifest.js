8a7cb5ff3d2329e888a45b0d6cb84fd38903e728
Ran Mizrahi <ran@bit.dev>
2020-02-17 18:50:12 +0200

    working extension

--- extensions/react/react.manifest.js
+++ extensions/react/react.manifest.js
@@ -3,8 +3,12 @@ const { ScriptsExt } = require('bit-bin/dist/extensions/scripts');
 module.exports = {
   name: 'extensions/react-ts',
   dependencies: [ScriptsExt],
+  config: {
+
+  },
   provider: async (config, [scripts]) => {
     scripts.register({name: 'extensions/react-ts'}, './transpile');
+    // scripts.register('./transpile', 'foo');
     return {};
   }
 };
