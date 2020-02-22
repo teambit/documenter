71acdabbedd56a7e4c3ea93cf0b4a4b48814821b
David First <david@bitsrc.io>
2020-02-21 19:35:13 -0500

    change transpile script to export a function in order to support the latest changes in bit-bin scripts

--- extensions/react/transpile.js
+++ extensions/react/transpile.js
@@ -5,19 +5,25 @@ const typescript = require('typescript');
 const merge = require('merge2');
 const tsconfig = require('./default-tsconfig');
 
-const tsProject = ts.createProject(tsconfig);
 
-gulp.task('default', () => {
-  const tsResult = gulp
-    .src(['*.ts', '*.tsx'])
-    .pipe(tsProject())
+function transpile() {
+  const tsProject = ts.createProject(tsconfig);
 
-  return merge([
-    tsResult.dts.pipe(gulp.dest('dist')),
-    tsResult.js.pipe(gulp.dest('dist'))
-  ]);
-});
+  gulp.task('default', () => {
+    const tsResult = gulp
+      .src(['*.ts', '*.tsx'])
+      .pipe(tsProject())
 
-gulp.series('default')((err) => {
-  if (err) console.error(err);
-});
+    return merge([
+      tsResult.dts.pipe(gulp.dest('dist')),
+      tsResult.js.pipe(gulp.dest('dist'))
+    ]);
+  });
+
+  gulp.series('default')((err) => {
+    if (err) console.error(err);
+  });
+  return { dir: 'dist' };
+}
+
+module.exports = transpile;
