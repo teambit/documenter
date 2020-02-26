0e3650f4a63584a0069087232eaccd5315dad102
David First <david@bitsrc.io>
2020-02-26 17:55:43 -0500

    configure the new "create" extension to get templates from "extensions/react-ts"

--- extensions/react/react.manifest.js
+++ extensions/react/react.manifest.js
@@ -1,15 +1,27 @@
 const { ScriptsExt } = require('bit-bin/extensions/scripts');
+const { CreateExt } = require('bit-bin/extensions/create');
 
 module.exports = {
   name: 'extensions/react-ts',
-  dependencies: [ScriptsExt],
+  dependencies: [ScriptsExt, CreateExt],
   config: {
 
   },
-  provider: async (config, [scripts]) => {
+  provider: async (config, [scripts, create]) => {
     scripts.register({name: 'extensions/react-ts'}, './transpile');
+    create.register({ name: 'extensions/react-ts' }, getTemplates);
     return {
-      
+
     };
   }
 };
+
+function getTemplates(name) {
+  return {
+    files: [
+      { path: `${name}.js`, content: `export default function ${name}() { console.log('hello react'); }` },
+      { path: `${name}.spec.js`, content: `export default function ${name}() { console.log('hello test'); }` }
+    ],
+    main: `${name}.js`
+  };
+}
