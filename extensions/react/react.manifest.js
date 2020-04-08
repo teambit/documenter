d9977638a50e7a93321a93f8dda054bd7df66892
Gilad Shoham <shoham.gilad@gmail.com>
2020-04-08 13:16:08 +0300

    add example of extension configured by extension

--- extensions/react/react.manifest.js
+++ extensions/react/react.manifest.js
@@ -1,23 +1,33 @@
 const { CreateExt } = require('bit-bin/extensions/create');
 const { WorkspaceConfigExt } = require('bit-bin/extensions/workspace-config');
+const {ComponentFactoryExt} = require('bit-bin');
 
 const EXT_NAME = 'extensions/react-ts';
 
 module.exports = {
   name: EXT_NAME,
-  // dependencies: [ CreateExt, WorkspaceConfigExt],
-  dependencies: [ WorkspaceConfigExt],
+  // dependencies: [ CreateExt, WorkspaceConfigExt, component],
+  dependencies: [ WorkspaceConfigExt, ComponentFactoryExt],
   config: {
 
   },
-  // provider: async ([create, workspaceConfig]) => {
-  provider: async ([workspaceConfig]) => {
+  // provider: async ([create, workspaceConfig, component]) => {
+  provider: async ([workspaceConfig, component]) => {
     const myConfig = workspaceConfig.workspaceSettings.getExtensionConfig(EXT_NAME);
     // create.register({ name: EXT_NAME }, getTemplates);
     console.log('myConfig', myConfig)
-    return {
-
-    };
+    component.registerAddConfig(EXT_NAME, config => {
+      console.log('config registration hook is running for react');
+      return {
+        'some-key-added-by-react': 'some-val-added-by-react',
+        extensions: {
+          'extensions/dummy': {
+            'some-key-for-dummy': 'some-val-for-dummy'
+          }
+        }
+      };
+    });
+    return {};
   }
 };
 
