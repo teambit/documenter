613b3a3084adfa1c4bd9bfbbf71dd2a08aa14d72
oded <oded@bit.dev>
2020-07-13 15:48:47 +0300

    table heading row now uses table heading column

--- components/extension-components/table-heading-row/table-heading-row.tsx
+++ components/extension-components/table-heading-row/table-heading-row.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import classNames from "classnames";
-import { H5 } from "@bit/bit.test-scope.ui.heading";
-import { PossibleSizes } from "@bit/bit.base-ui.theme.sizes";
 import { Grid } from "@bit/bit.base-ui.layout.grid-component";
 import { ColNumber } from "@bit/bit.test-scope.ui.table/table";
+import { HeadingColumn } from "@bit/bit.test-scope.ui.table-heading-column";
 import styles from "./table-heading-row.module.scss";
 
 export type HeadingRowProps = {
@@ -17,13 +16,10 @@ export function HeadingRow({headings, colNumber}: HeadingRowProps) {
       {headings.map((title: string, index: number) => {
         if (title === "required") return;
         return (
-          <H5
-            key={index}
-            className={classNames(styles.title)}
-            size={PossibleSizes.xxs}
-          >
+          <HeadingColumn key={index}>
             {title}
-          </H5>
+          </HeadingColumn>
+          
         );
       })}
     </Grid>
