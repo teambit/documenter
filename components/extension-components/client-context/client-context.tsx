6680f31ac61fafda7103a06aa4c3319f810d09e9
oded <oded@bit.dev>
2020-07-14 17:33:43 +0300

    aligned docs to new design

--- components/extension-components/client-context/client-context.tsx
+++ components/extension-components/client-context/client-context.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import classNames from 'classnames';
 import { Theme } from "@bit/bit.base-ui.theme.theme-provider";
-import styles from "./sizes.module.scss";
+import sizes from "./sizes.module.scss";
+import global from './global.module.scss'; // TODO - rename
 
 type ClientContextProps = {
   children: JSX.Element;
@@ -8,8 +10,9 @@ type ClientContextProps = {
 
 export function ClientContext({ children, ...rest }: ClientContextProps) {
   return (
-      <Theme {...rest} className={styles.heading}>
+      <Theme {...rest} className={classNames(sizes.heading, global.overrides)}>
         {/* // dev link for icons */}
+        {/* TODO - decide if icons link needs to be here or separated */}
         <link
           rel="stylesheet"
           href="https://i.icomoon.io/public/9dc81da9ad/Bit/style.css"
