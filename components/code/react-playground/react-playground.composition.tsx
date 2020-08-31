d9b6129edb5a052ef93706b1b9d17c937060f166
oded <oded@bit.dev>
2020-08-31 15:28:57 +0300

    react playground styling

--- components/code/react-playground/react-playground.composition.tsx
+++ components/code/react-playground/react-playground.composition.tsx
@@ -5,10 +5,28 @@ import { ThemeContext } from '@teambit/documenter.theme.theme-context';
 
 const codeString = `<div>hello playground</div>`;
 
+const functionString = `
+() => {
+  const Button = ({ children }) => (
+      <button >{children}</button>
+  );
+  const scope = { Button };
+  const code = "<Button>my first playground button</Button>";
+      
+  return <button>sfgsdfg </button>
+}`
+
 export const HelloWorld = () => {
   return (
     <ThemeContext>
       <Playground code={codeString} />
+    </ThemeContext>
+  );
+};
+export const FunctionExmple = () => {
+  return (
+    <ThemeContext>
+      <Playground code={functionString} />
     </ThemeContext>
   );
 };
--- components/code/react-playground/react-playground.tsx
+++ components/code/react-playground/react-playground.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import classNames from 'classnames';
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
-
-import prismTheme from 'prism-react-renderer/themes/github';
-import { Error as errorClass } from '@teambit/base-ui.input.error';
-import { roundnessClass } from '@teambit/base-ui.css-components.roundness';
-
-import styles from './react-playground.module.scss';
+import React from "react";
+import classNames from "classnames";
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from "react-live";
+import prismTheme from "prism-react-renderer/themes/oceanicNext";
+import { Error as errorClass } from "@teambit/base-ui.input.error";
+import styles from "./react-playground.module.scss";
 
 export type CodeScope = { [key: string]: any };
 
@@ -20,13 +22,18 @@ export type PlaygroundProps = {
 export function Playground({ code, scope }: PlaygroundProps) {
   return (
     <LiveProvider code={code} scope={scope} theme={prismTheme}>
-      <LivePreview
-        className={classNames(styles.preview, roundnessClass.default)}
-      />
-      <LiveError className={classNames(errorClass, styles.error)} />
-      <LiveEditor
-        className={classNames(roundnessClass.default, styles.editor)}
-      />
+      <div className={styles.playground}>
+        <LiveEditor
+          className={classNames(styles.editor)}
+          style={{fontFamily: 'Roboto mono'}}
+        />
+        <div className={styles.previewBox}>
+          <LivePreview
+            className={classNames(styles.preview)}
+          />
+          <LiveError className={classNames(errorClass, styles.error)} />
+        </div>
+      </div>
     </LiveProvider>
   );
 }
