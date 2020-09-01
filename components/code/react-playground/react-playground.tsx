5b4872178ad90f099a42ecf2a8d60a25599c20b9
oded <oded@bit.dev>
2020-09-01 15:45:55 +0300

    playground - added split pane

--- components/code/react-playground/react-playground.tsx
+++ components/code/react-playground/react-playground.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import classNames from "classnames";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import {
-  LiveProvider,
-  LiveEditor,
-  LiveError,
-  LivePreview
-} from "react-live";
+  SplitPane,
+  Pane,
+  Layout,
+} from "@teambit/base-ui.surfaces.split-pane.split-pane";
+import { HoverSplitter } from "@teambit/base-ui.surfaces.split-pane.hover-splitter";
+import useDimensions from "react-use-dimensions";
+import { useDebounce } from "use-debounce";
 import prismTheme from "prism-react-renderer/themes/oceanicNext";
 import { Error as errorClass } from "@teambit/base-ui.input.error";
 import styles from "./react-playground.module.scss";
@@ -20,19 +23,32 @@ export type PlaygroundProps = {
 };
 
 export function Playground({ code, scope }: PlaygroundProps) {
+  const [ref, { width }] = useDimensions();
+  const debouncedSize = useDebounce(width, 300);
+  const placement =
+    +debouncedSize[0] <= 1000 ? Layout.columnReverse : Layout.row;
+
   return (
     <LiveProvider code={code} scope={scope} theme={prismTheme}>
-      <div className={styles.playground}>
-        <LiveEditor
-          className={classNames(styles.editor)}
-          style={{fontFamily: 'Roboto mono'}}
-        />
-        <div className={styles.previewBox}>
-          <LivePreview
-            className={classNames(styles.preview)}
-          />
-          <LiveError className={classNames(errorClass, styles.error)} />
-        </div>
+      <div ref={ref} className={styles.playground}>
+        <SplitPane className={styles.main} size={"50%"} layout={placement}>
+          <Pane className={styles.leftPane}>
+            <div className={styles.editorWrapper}>
+              <LiveEditor
+                className={classNames(styles.editor)}
+                style={{ fontFamily: "Roboto mono" }}
+              />
+            </div>
+          </Pane>
+          <HoverSplitter className={styles.splitter}></HoverSplitter>
+          <Pane className={styles.rightPane}>
+            <div className={styles.previewBox}>
+              <LivePreview /> 
+              {/* //className={classNames(styles.preview)} /> */}
+              <LiveError className={classNames(errorClass, styles.error)} />
+            </div>
+          </Pane>
+        </SplitPane>
       </div>
     </LiveProvider>
   );
