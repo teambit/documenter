3e86814d6bc662c926a763f70cca309e6151d3b3
Josh Kuttler <yoshuakuttler@gmail.com>
2020-12-31 23:43:52 +0200

    add theme-compositions component and update theme-context

---
+++ components/theme-compositions/theme-compositions.compositions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ThemeCompositions } from './theme-compositions';
+
+export const CenterThemeContextExample = () => (
+  <ThemeCompositions>
+    <div>This is a center element</div>
+  </ThemeCompositions>
+);
+
+CenterThemeContextExample.canvas = {
+  height: 200,
+};
---
+++ components/theme-compositions/theme-compositions.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+import { EvaIconFont } from '@teambit/evangelist.theme.icon-font';
+import {
+  ThemeDocumenter,
+  ThemeDocumenterProps,
+} from '@teambit/documenter.theme.theme-context';
+
+import styles from './theme-compositions.module.scss'; // TODO - rename
+
+const ICON_MOON_VERSION = 'mxd7i0';
+
+export type ThemeCompositionsProps = {} & ThemeDocumenterProps;
+
+export const ThemeCompositions = ({
+  children,
+  ...rest
+}: ThemeCompositionsProps) => {
+  return (
+    <ThemeDocumenter {...rest} className={classNames(styles.center)}>
+      <EvaIconFont query={ICON_MOON_VERSION} />
+      {children}
+    </ThemeDocumenter>
+  );
+};
