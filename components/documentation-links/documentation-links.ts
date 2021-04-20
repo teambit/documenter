62aca2700a81601e96c6aaecf4a0814e4a023544
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-04-20 19:50:47 +0200

    fix: update docs links

--- components/documentation-links/documentation-links.ts
+++ components/documentation-links/documentation-links.ts
@@ -1,11 +1,11 @@
-export const docsUrl = 'https://harmony-docs.bit.dev'
+export const docsUrl = 'https://harmony-docs.bit.dev';
 
 export const links = {
-  scopedRegistry: `${docsUrl}/components/installing`,
-  installBit: `${docsUrl}/introduction/installation`,
-  createCompositions: `${docsUrl}/tutorial/render-component`,
-  testComponents: `${docsUrl}/tutorial/test`,
-  bitIntroduction: `${docsUrl}/introduction/getting-started`,
-  whatIsBit: `${docsUrl}/introduction/why-bit`,
-  tagComponentVersion: `${docsUrl}/tutorial/version`
-}
+  scopedRegistry: `${docsUrl}/building-with-bit/installing-components`,
+  installBit: `${docsUrl}/getting-started/installing-bit`,
+  createCompositions: `${docsUrl}/building-with-bit/component-compositions`,
+  testComponents: `${docsUrl}/building-with-bit/testing-components`,
+  bitIntroduction: `${docsUrl}/getting-started/installing-bit`,
+  whatIsBit: `${docsUrl}/essentials/what-is-bit`,
+  tagComponentVersion: `${docsUrl}/building-with-bit/versioning-components`
+};
