e06c295ec4dfdd6fa01d3ae63e5bf3aa964dc9f0
Josh Kuttler <yoshuakuttler@gmail.com>
2021-05-30 22:14:31 +0300

    resolve conflict

--- components/list/list.docs.tsx
+++ components/list/list.docs.tsx
@@ -62,3 +62,4 @@ export const examples = [
 </ThemeCompositions>
 `}
 ]
+
--- components/table/table.docs.tsx
+++ components/table/table.docs.tsx
@@ -4,3 +4,4 @@ import { Table } from "@teambit/documenter.ui.table";
 import { List } from "@teambit/documenter.ui.list";
 
 export const labels = ['react', 'ui-component', 'table'];
+
