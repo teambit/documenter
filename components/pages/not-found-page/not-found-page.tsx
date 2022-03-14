551789a5df63b359e598a34b5d1431a740885ef5
Josh Kuttler <35892475+JoshK2@users.noreply.github.com>
2022-03-14 19:22:57 +0200

    update docs links to new docs (#28)

--- components/pages/not-found-page/not-found-page.tsx
+++ components/pages/not-found-page/not-found-page.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { Icon } from "@teambit/evangelist.elements.icon";
-import { ErrorPage } from "@teambit/documenter.pages.error";
-import { Image } from "@teambit/evangelist.elements.image";
-import { ExternalLink } from "@teambit/documenter.routing.external-link";
-import styles from "./not-found-page.module.scss";
+import React from 'react';
+import { Icon } from '@teambit/evangelist.elements.icon';
+import { ErrorPage } from '@teambit/documenter.pages.error';
+import { Image } from '@teambit/evangelist.elements.image';
+import { ExternalLink } from '@teambit/documenter.routing.external-link';
+import styles from './not-found-page.module.scss';
 
 type NotFoundPageProps = {} & React.HTMLAttributes<HTMLDivElement>;
 
 /**
  * A component that shows a 404 error page
  */
-export function NotFoundPage({
-  className,
-  children,
-  ...rest
-}: NotFoundPageProps) {
+export function NotFoundPage({ className, children, ...rest }: NotFoundPageProps) {
   return (
     <ErrorPage {...rest} code={404} title="Page not found">
       <div className={styles.iconLine}>
@@ -24,11 +20,10 @@ export function NotFoundPage({
         <ExternalLink href="https://github.com/teambit/bit">
           <Icon of="github-logo" className={styles.github} />
         </ExternalLink>
-        <ExternalLink href="https://docs.bit.dev/docs/quick-start">
+        <ExternalLink href="https://bit.dev/docs">
           <Image alt="bit-logo" className={styles.logo} src="bit-logo.svg" />
         </ExternalLink>
       </div>
     </ErrorPage>
   );
 }
-
